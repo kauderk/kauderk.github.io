d97829529725ae50dd2bc7745a1d4e628519e91e
KauDerK <estudio1kau@gmail.com>
2021-11-08 21:30:47 -0500

    LoadExternalResources bugs 6

--- yt-gif-extension/version-0.1/js/yt-gif-main.js
+++ yt-gif-extension/version-0.1/js/yt-gif-main.js
@@ -168,49 +168,78 @@ rm_components.both = {
 let Utils;
 (async function ()
 {
+    debugger;
     await LoadExternalResources();
-    Utils = kauderk.util;
-    try
-    {
-        await Ready();
-    } catch (error)
-    {
-        debugger;
-    }
+    Utils = kauderk.Utils;
+    await Ready();
+    debugger;
 })();
+
 async function LoadExternalResources()
 {
-    if (typeof kauderk !== 'undefined' &&
+    if (
+        typeof kauderk !== 'undefined' &&
         typeof kauderk.util !== 'undefined' &&
-        typeof (YT) !== 'undefined')
+        typeof (YT) != 'undefined'
+    )
     {
         return null;
     }
     else
     {
-        const utilsScript = document.createElement("script");
-        utilsScript.src = URLFolderJS(`utils.js`);
-        utilsScript.id = 'yt-gif-utils';
-        utilsScript.type = "text/javascript";
-        document.getElementsByTagName('head')[0].appendChild(utilsScript);
-
         await loadYT_IFRAME_API();
-        await loadScript(utilsScript);
+        await createXload('utils.js');
 
         return null;
+    }
+    function loadYT_IFRAME_API()
+    {
+        const script = document.createElement('script');
+        script.src = 'https://www.youtube.com/player_api';
+        const firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(script, firstScriptTag);
+        return new Promise((resolve, reject) => { script.onload = () => resolve(script) })
+    }
+}
+async function createXload(src)
+{
+    const obj = {
+        src: src,
+        id: `script-yt_gif-${src}`
+    }
 
-        async function loadScript(script)
+    romoveIfany(obj.id);
+    const script = createScript(obj);
+    return await loadScript(script);
+
+    function romoveIfany(id)
+    {
+        const scriptAlready = document.querySelectorAll(`[id='${id}']`);
+        if (scriptAlready) // well well well - we don't like duplicates - lol
         {
-            return new Promise((resolve, reject) => { script.onload = () => resolve(script) })
+            for (const el of scriptAlready)
+            {
+                el.parentElement.removeChild(el);
+            }
         }
-        async function loadYT_IFRAME_API()
+    }
+    function createScript({ src, id })
+    {
+        const script = document.createElement("script");
+        script.src = URLFolderJS(src) + "?" + new Date().getTime();
+        script.id = id;
+        script.async = false;
+        script.type = "text/javascript";
+        document.getElementsByTagName('head')[0].appendChild(script);
+        return script;
+    }
+    async function loadScript(script)
+    {
+        return new Promise((resolve, reject) =>
         {
-            const script = document.createElement('script');
-            script.src = 'https://www.youtube.com/player_api';
-            const firstScriptTag = document.getElementsByTagName('script')[0];
-            firstScriptTag.parentNode.insertBefore(script, firstScriptTag);
-            return await loadScript(script);
-        }
+            //Script finished loading
+            script.addEventListener('load', () => resolve(script))
+        })
     }
 }
 
