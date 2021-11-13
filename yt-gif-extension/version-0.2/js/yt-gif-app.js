6e44d90ca13d07c6b988b1d810d30999ddadcade
KauDerK <estudio1kau@gmail.com>
2021-11-13 10:07:09 -0500

    HTML 3 - testing yt gif tutorial - drop down menu

--- yt-gif-extension/version-0.2/js/yt-gif-app.js
+++ yt-gif-extension/version-0.2/js/yt-gif-app.js
@@ -124,7 +124,7 @@ const links = {
         }
     },
     html: {
-        dropDownMenu: URLFolderHTML('testing/html/drop-down-menu.html'),
+        dropDownMenu: URLFolder('testing/html/drop-down-menu.html'),
         playerControls: URLFolderHTML('player-controls.html'),
         fetched: {
             playerControls: '',
