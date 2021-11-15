061089f4f347fa390ab8f3dd24538ac1c5eff1ef
KauDerK <estudio1kau@gmail.com>
2021-11-15 06:10:46 -0500

    1 - version-0.1-update - restricting some features and borrowing resources from version-0.2

---
+++ yt-gif-extension/version-0.1-update/yt-gif-main.js
@@ -0,0 +1,87 @@
+
+(async function ()
+{
+    await LoadExternalResources();
+    //await window.YT_GIF_SETTINGS_PAGE_INIT();
+    await createXload('yt-gif-app.js', 'https://kauderk.github.io/yt-gif-extension/version-0.1-update/js/yt-gif-app.js');
+})();
+
+async function LoadExternalResources()
+{
+    if (
+        typeof kauderk !== 'undefined' &&
+        typeof kauderk.util !== 'undefined' &&
+        typeof (kauderk.rap) != 'undefined' &&
+        typeof (YT) != 'undefined'
+    )
+    {
+        return null;
+    }
+    else
+    {
+        await loadYT_IFRAME_API();
+
+        await createXload('utils.js');
+
+        await createXload('utils-roam-alpha-api.js');
+
+        //await createXload('settings-page.js');
+
+        return null;
+    }
+    function loadYT_IFRAME_API()
+    {
+        const script = document.createElement('script');
+        script.src = 'https://www.youtube.com/player_api';
+        const firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(script, firstScriptTag);
+        return new Promise((resolve, reject) => { script.onload = () => resolve(script) })
+    }
+}
+async function createXload(src, fullSrc)
+{
+    const obj = {
+        src,
+        id: `script-yt_gif-${src}`,
+        fullSrc
+    }
+
+    romoveIfany(obj.id);
+    const script = createScript(obj);
+    return await loadScript(script);
+
+    function romoveIfany(id)
+    {
+        const scriptAlready = document.querySelectorAll(`[id='${id}']`);
+        if (scriptAlready) // well well well - we don't like duplicates - lol
+        {
+            for (const el of scriptAlready)
+            {
+                el.parentElement.removeChild(el);
+            }
+        }
+    }
+    function createScript({ src, id, fullSrc })
+    {
+        const script = document.createElement("script");
+        const preSrc = fullSrc || URLFolderJS(src);
+        script.src = preSrc + "?" + new Date().getTime();
+        script.id = id;
+        script.async = false;
+        script.type = "text/javascript";
+        document.getElementsByTagName('head')[0].appendChild(script);
+        return script;
+        function URLFolderJS(f)
+        {
+            return `https://kauderk.github.io/yt-gif-extension/version-0.2/js/${f}`
+        };
+    }
+    async function loadScript(script)
+    {
+        return new Promise((resolve, reject) =>
+        {
+            //Script finished loading
+            script.addEventListener('load', () => resolve(script))
+        })
+    }
+}
