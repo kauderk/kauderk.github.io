ef32deab57eefba87a16e8257b49eae6bc73b97a
KauDerK <estudio1kau@gmail.com>
2021-11-27 23:43:37 -0500

    utils convertHMS HMSToSecondsOnly

--- yt-gif-extension/resources/js/utils.js
+++ yt-gif-extension/resources/js/utils.js
@@ -2,7 +2,19 @@ var kauderk = window.kauderk || {};
 
 kauderk.util = ((util) =>
 {
-    util.hmsToSecondsOnly = (str) =>
+    util.convertHMS = (value) =>
+    {
+        const sec = parseInt(value, 10); // convert value to number if it's string
+        let hours = Math.floor(sec / 3600); // get hours
+        let minutes = Math.floor((sec - (hours * 3600)) / 60); // get minutes
+        let seconds = sec - (hours * 3600) - (minutes * 60); //  get seconds
+        // add 0 if value < 10; Example: 2 => 02
+        if (hours < 10) { hours = "0" + hours; }
+        if (minutes < 10) { minutes = "0" + minutes; }
+        if (seconds < 10) { seconds = "0" + seconds; }
+        return hours + ':' + minutes + ':' + seconds; // Return is HH : MM : SS
+    }
+    util.HMSToSecondsOnly = (str) =>
     {
         var p = str.split(':'),
             s = 0, m = 1;
