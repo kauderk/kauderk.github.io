82631202326734152c75c10e71f0db99e01006d4
KauDerK <estudio1kau@gmail.com>
2021-11-27 21:02:38 -0500

    utils.hmsToSecondsOnly

--- yt-gif-extension/resources/js/utils.js
+++ yt-gif-extension/resources/js/utils.js
@@ -2,6 +2,19 @@ var kauderk = window.kauderk || {};
 
 kauderk.util = ((util) =>
 {
+    util.hmsToSecondsOnly = (str) =>
+    {
+        var p = str.split(':'),
+            s = 0, m = 1;
+
+        while (p.length > 0)
+        {
+            s += m * parseInt(p.pop(), 10);
+            m *= 60;
+        }
+
+        return s;
+    }
     util.simHover = () =>
     {
         return new MouseEvent('mouseenter',
