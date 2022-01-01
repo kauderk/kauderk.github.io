482b740a85fa7e393ff030d7de7d7b82135e79d6
KauDerK <estudio1kau@gmail.com>
2022-01-01 15:01:41 -0500

    1 js util getBlockInfoByUIDM - open order string uid

--- yt-gif-extension/resources/js/utils-roam-alpha-api.js
+++ yt-gif-extension/resources/js/utils-roam-alpha-api.js
@@ -152,6 +152,26 @@ kauderk.rap = ((rap) =>
     {
         return await window.roamAlphaAPI.q(`[:find (pull ?b [:block/string]):where [?b :block/uid "${blockUid}"]]`);
     }
+    rap.getBlockInfoByUIDM = async (uid, withChildren = false, withParents = false) =>
+    {
+        try
+        {
+            let q = `[:find (pull ?page
+                         [:node/title :block/string :block/uid :block/heading :block/props 
+                          :entity/attrs :block/open :block/text-align :children/view-type
+                          :block/order
+                          ${withChildren ? '{:block/children ...}' : ''}
+                          ${withParents ? '{:block/parents ...}' : ''}
+                         ])
+                      :where [?page :block/uid "${uid}"]  ]`;
+            var results = await window.roamAlphaAPI.q(q);
+            if (results.length == 0) return null;
+            return results;
+        } catch (e)
+        {
+            return null;
+        }
+    }
     rap.sortObjectsByOrder = (o) =>
     {
         return o.sort((a, b) => a.order - b.order);
